cb09eb763ea2bd25b02bde574f0e772309d131aa
Tangwan tech <tangwantech@gmail.com>
2025-02-22 16:00:05 +0100

    Subcription featured

--- app/src/main/java/com/example/gceolmcqs/SubscriptionActivity.kt
+++ app/src/main/java/com/example/gceolmcqs/SubscriptionActivity.kt
@@ -30,13 +30,15 @@ class SubscriptionActivity: AppCompatActivity(),
     PaymentMethodDialogFragment.OnPaymentMethodDialogListeners,
     PackagesDialogFragment.PackageDialogListener{
 
-    private var processingAlertDialog: AlertDialog? = null
-    private var requestToPayDialog: AlertDialog? = null
-    private var failedToActivatePackageDialog: AlertDialog? = null
-    private var activatingPackageDialog: AlertDialog? = null
-    private var packageActivatedDialog: AlertDialog? = null
-    private var paymentReceivedDialog: AlertDialog? = null
-    private var momoNumberInputDialog: AlertDialog? = null
+//    private var processingAlertDialog: AlertDialog? = null
+//    private var requestToPayDialog: AlertDialog? = null
+//    private var failedToActivatePackageDialog: AlertDialog? = null
+//    private var activatingPackageDialog: AlertDialog? = null
+//    private var packageActivatedDialog: AlertDialog? = null
+//    private var paymentReceivedDialog: AlertDialog? = null
+//    private var momoNumberInputDialog: AlertDialog? = null
+
+    private var dialog: AlertDialog? = null
 
     private var packagesDialog: DialogFragment? = null
     private var paymentMethodDialog: DialogFragment? = null
@@ -73,13 +75,7 @@ class SubscriptionActivity: AppCompatActivity(),
 
             status?.let{
                 if (it){
-                    if(activatingPackageDialog != null){
-                        cancelActivatingPackageDialog()
-                    }
-                    if (packageActivatedDialog == null){
-                        showPackageActivatedDialog()
-                    }
-
+                    showPackageActivatedDialog()
                 }
 
             }
@@ -98,49 +94,22 @@ class SubscriptionActivity: AppCompatActivity(),
             it?.let{status ->
                 when(status) {
                     MCQConstants.PENDING -> {
-                        if (processingAlertDialog != null){
-                            cancelProcessingRequestDialog()
-                        }
-                        if (failedToActivatePackageDialog != null){
-                            cancelFailedToActivateDialog()
-                        }
-
-                        if (requestToPayDialog == null){
-                            showRequestUserToPayDialog()
-                        }
+                        showRequestUserToPayDialog()
 
                     }
                     MCQConstants.SUCCESSFUL -> {
-                        cancelProcessingAndRequestToPayDialogs()
-
-                        if(paymentReceivedDialog == null){
-                            showPaymentReceivedDialog()
-
-                        }
-                        if (failedToActivatePackageDialog != null){
-                            cancelFailedToActivateDialog()
-                        }
+                        showPaymentReceivedDialog()
 
                         CoroutineScope(Dispatchers.IO).launch {
                             delay(2000)
                             withContext(Dispatchers.Main){
-                                if(paymentReceivedDialog != null){
-                                    cancelPaymentReceivedDialog()
-                                }
-                                if(activatingPackageDialog == null){
-                                    showActivatingPackageDialog()
-                                    activateUserPackage()
-                                }
+                                showActivatingPackageDialog()
+                                activateUserPackage()
                             }
                         }
                     }
                     MCQConstants.FAILED -> {
-                        cancelProcessingAndRequestToPayDialogs()
-
-                        if(failedToActivatePackageDialog == null){
-                            showTransactionFailedDialog()
-
-                        }
+                        showTransactionFailedDialog()
                     }
                 }
             }
@@ -153,35 +122,27 @@ class SubscriptionActivity: AppCompatActivity(),
 
 
     private fun showProcessingRequestDialog() {
-        processingAlertDialog = AlertDialog.Builder(this).create()
-        processingAlertDialog?.apply {
-            setCancelable(false)
+        if (dialog != null){
+            dialog?.dismiss()
         }
-        processingAlertDialog?.setMessage(resources.getString(R.string.processing_request))
-        processingAlertDialog?.show()
-    }
-
-    private fun cancelProcessingRequestDialog() {
-        processingAlertDialog?.dismiss()
-        processingAlertDialog = null
+        dialog = AlertDialog.Builder(this).apply {
+            setMessage(resources.getString(R.string.processing_request))
+            setCancelable(false)
+        }.create()
+        dialog?.show()
     }
 
     private fun showActivatingPackageDialog() {
-        activatingPackageDialog = AlertDialog.Builder(this).create()
-        activatingPackageDialog?.apply {
-            setCancelable(false)
+        if (dialog != null){
+            dialog?.dismiss()
         }
-        activatingPackageDialog?.setMessage(resources.getString(R.string.activating_package_message))
-        activatingPackageDialog?.show()
+        dialog = AlertDialog.Builder(this).apply {
+            setMessage(resources.getString(R.string.activating_package_message))
+            setCancelable(false)
+        }.create()
+        dialog?.show()
 //        activateUserPackage()
     }
-
-    private fun cancelActivatingPackageDialog() {
-        activatingPackageDialog?.dismiss()
-        activatingPackageDialog = null
-    }
-
-
 
     private fun showRequestUserToPayDialog() {
 
@@ -210,30 +171,18 @@ class SubscriptionActivity: AppCompatActivity(),
         tvRequestToPayPackagePrice.text = "${viewModel.getPackagePrice()} FCFA"
 //        tvTransactionId.text = "Reference Number: $currentRefNum"
 
-        requestToPayDialog = AlertDialog.Builder(this).create()
-        requestToPayDialog?.apply {
+        if (dialog != null){
+            dialog?.dismiss()
+        }
+
+        dialog = AlertDialog.Builder(this).apply {
+            setView(dialogView)
             setCancelable(false)
-        }
-        requestToPayDialog?.setView(dialogView)
-        requestToPayDialog?.show()
+        }.create()
+        dialog?.show()
 
     }
 
-    private fun cancelRequestToPayDialog() {
-//        requestToPayDialog.cancel()
-        requestToPayDialog?.dismiss()
-        requestToPayDialog =  null
-    }
-
-    private fun cancelProcessingAndRequestToPayDialogs(){
-        if(processingAlertDialog != null){
-            cancelProcessingRequestDialog()
-        }
-
-        if(requestToPayDialog != null){
-            cancelRequestToPayDialog()
-        }
-    }
 
     private fun showPackageActivatedDialog() {
         val view = layoutInflater.inflate(
@@ -244,58 +193,53 @@ class SubscriptionActivity: AppCompatActivity(),
             view.findViewById(R.id.tvPackageActivationSuccessful)
         tvPackageActivationSuccessful.text =
             "${viewModel.getSubjectPackageType()} ${resources.getString(R.string.activated_successfully)}"
-        val packageActivatedDialog = AlertDialog.Builder(this).apply {
+
+        if (dialog != null){
+            dialog?.dismiss()
+        }
+        dialog = AlertDialog.Builder(this).apply {
             setView(view)
             setPositiveButton("Ok"){ _, _ ->
             exitActivity()
             }
             setCancelable(false)
         }.create()
-        packageActivatedDialog.show()
+        dialog?.show()
     }
 
     private fun showTransactionFailedDialog() {
-//        if(failedToActivatePackageDialog != null){
-//            failedToActivatePackageDialog?.cancel()
-//        }
-        failedToActivatePackageDialog = AlertDialog.Builder(this).create()
-
         val view = layoutInflater.inflate(R.layout.package_activation_failed_dialog, null)
         val tvFailedMessage: TextView = view.findViewById(R.id.tvPackageActivationFailed)
         tvFailedMessage.text =
             "${resources.getString(R.string.failed_to_activate_package)} ${viewModel.getSubjectPackageType()} "
 
-//        failedToActivatePackageDialog?.setView(view)
-
-        failedToActivatePackageDialog?.apply {
+        if (dialog != null){
+            dialog?.dismiss()
+        }
+        dialog = AlertDialog.Builder(this).apply {
             setView(view)
             setCancelable(false)
-        }
+        }.create()
 
-        failedToActivatePackageDialog?.setButton(AlertDialog.BUTTON_POSITIVE, "Ok") { _, _ ->
-            cancelFailedToActivateDialog()
+        dialog?.setButton(AlertDialog.BUTTON_POSITIVE, "Ok") { _, _ ->
             exitActivity()
 
         }
 
-        failedToActivatePackageDialog?.show()
+        dialog?.show()
 
     }
 
-    private fun cancelFailedToActivateDialog(){
-        failedToActivatePackageDialog?.dismiss()
-        failedToActivatePackageDialog = null
-    }
+
 
     private fun showPaymentReceivedDialog(){
-        paymentReceivedDialog = AlertDialog.Builder(this).create()
-        paymentReceivedDialog?.setMessage(resources.getString(R.string.payment_received))
-        paymentReceivedDialog?.show()
-    }
-
-    private fun cancelPaymentReceivedDialog(){
-        paymentReceivedDialog?.dismiss()
-        paymentReceivedDialog = null
+        if (dialog != null){
+            dialog?.dismiss()
+        }
+        dialog = AlertDialog.Builder(this).apply {
+            setMessage(resources.getString(R.string.payment_received))
+        }.create()
+        dialog?.show()
     }
 
     private fun displayInvoice(){
@@ -309,21 +253,25 @@ class SubscriptionActivity: AppCompatActivity(),
         packageName.text = "${viewModel.getSubjectPackageType()}"
         packagePrice.text = "${viewModel.getPackagePrice()} FCFA"
         momoNumber.text = "${viewModel.getMomoNumber()}"
-        val dialog = AlertDialog.Builder(this)
-        dialog.apply{
+
+
+        if (dialog != null){
+            dialog?.dismiss()
+        }
+
+        dialog = AlertDialog.Builder(this).apply{
             setMessage(resources.getString(R.string.verify_payment_info))
             setView(view)
             setCancelable(false)
             setPositiveButton(resources.getString(R.string.pay)  ){btn, _ ->
                 showProcessingRequestDialog()
                 viewModel.initiatePayment()
-                btn.dismiss()
             }
             setNegativeButton(resources.getString(R.string.cancel)){btn, _ ->
                 exitActivity()
             }
         }.create()
-        dialog.show()
+        dialog?.show()
     }
 
     private fun showPackagesDialog(){
@@ -340,19 +288,25 @@ class SubscriptionActivity: AppCompatActivity(),
     private fun displayEnterMomoNumberDialog(){
         val view = layoutInflater.inflate(R.layout.momo_number_dialog, null)
         val etMoMoNumber: TextInputEditText = view.findViewById(R.id.etMomoNumber)
-        momoNumberInputDialog = AlertDialog.Builder(this).create()
-        momoNumberInputDialog?.setTitle(resources.getString(R.string.enter_number))
-        momoNumberInputDialog?.setView(view)
-        momoNumberInputDialog?.setCancelable(false)
-        momoNumberInputDialog?.setButton(AlertDialog.BUTTON_POSITIVE, resources.getString(R.string.next)){ _, _ ->
-            displayInvoice()
-        }
-        momoNumberInputDialog?.setButton(AlertDialog.BUTTON_NEGATIVE,resources.getString(R.string.cancel)){ _, _ ->
-            exitActivity()
-        }
-        momoNumberInputDialog?.show()
 
-        val btnPositive = momoNumberInputDialog?.getButton(AlertDialog.BUTTON_POSITIVE)
+        if (dialog != null){
+            dialog?.dismiss()
+        }
+        dialog = AlertDialog.Builder(this).apply {
+            setTitle(resources.getString(R.string.enter_number))
+            setView(view)
+            setCancelable(false)
+            setPositiveButton( resources.getString(R.string.next)){ _, _ ->
+                displayInvoice()
+            }
+            setNegativeButton(resources.getString(R.string.cancel)){ _, _ ->
+                exitActivity()
+            }
+        }.create()
+
+        dialog?.show()
+
+        val btnPositive = dialog?.getButton(AlertDialog.BUTTON_POSITIVE)
         btnPositive?.isEnabled = false
 
         etMoMoNumber.doOnTextChanged { text, _, _, _ ->
